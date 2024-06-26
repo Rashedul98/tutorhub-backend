a4492a2224dc1e11db6b797e620a9416504a589e
Rashedul Islam <thisisrashedul@gmail.com>
2024-06-27 01:03:17 +0600

    removed unnecessary comments

--- routes/signup.js
+++ routes/signup.js
@@ -9,12 +9,10 @@ router.post("/student-signup", userStudentController.userStudent);
 
 
 //Teachers:
-
 router.post("/teacher-signup", userTeacherController.userTeacher);
 router.post("/teacher-list", userStudentController.getTeacherBySubject);
 
 
 module.exports = router;
 
-//router.get("/allTeacherList", userTeacherlistController.getTeachers);
 
